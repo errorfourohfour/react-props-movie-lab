a852fba32dae37d38fbe37d881dfbf58e06fc7b7
Zach Brookman <zachbrookman48@gmail.com>
2021-06-10 20:04:01 -0400

    commit tree 901aa92dc41a8ceba887f9679dc8e87e5e9fcfa0

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,7 +6,14 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map((movie, idx) => <MovieCard  
+      key={idx} // passing a key property like this gets rid of the error saying "each children needs a unique key prop"
+      title= {movie.title} 
+      IMDBRating={movie.IMDBRating} 
+      genres={movie.genres}  
+      poster= {movie.poster}/>)
   }
+
 
   render() {
     return (
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -27,17 +27,31 @@ const posterMap = {
   'default': defaultPoster
 }
 
-export default class MovieCard extends Component {
-
+class MovieCard extends Component{
+  // mapPoster = () => {
+  //      for(const key in posterMap){
+  //        if(key === this.props.poster){
+  //          return posterMap[key]
+  //        }
+  //      }
+  // }
   render() {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster= {posterMap[this.props.poster]}/>
+        <CardBack title={this.props.title} genres= {this.props.genres} IMDBRating = {this.props.IMDBRating}/>
       </div>
     )
   }
-}
+} 
+
+export default MovieCard
 
 // Don't forget your default props!
+MovieCard.defaultProps= {
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default'
+}
